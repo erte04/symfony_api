1b6452c80c9a1722bc1f61a32ffbb20656a5d494
Robert Tytko <kontakt@roberttytko.pl>
2019-11-04 22:00:18 +0100

    add oauth server

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,6 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    FOS\OAuthServerBundle\FOSOAuthServerBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
